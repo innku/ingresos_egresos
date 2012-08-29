da1ded8d0d44271de8eab2476ced048fc674f173
Elías Matheus <eliasmatheus@Eliass-MacBook-Air.local>
2012-08-29 15:42:25 -0500

    Initiliazed repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IngresosEgresos::Application.config.secret_token = '499f16b6cd13e3e96110a1f78e2e2b70dc36c59dbb071b853c6dd3e1165ae6a5b1560556bec4e5f999b756e8b0b31f335ba314f198df50ce1cc41279807b6573'
